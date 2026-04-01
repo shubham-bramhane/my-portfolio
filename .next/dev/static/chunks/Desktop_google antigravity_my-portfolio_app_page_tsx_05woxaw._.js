fda05de35246154ec1670cfbc03565a3791f18f9
shubham bramhane <shubham.bramhane@abym.in>
2026-04-01 17:09:23 +0530

    new code

--- .next/dev/static/chunks/Desktop_google antigravity_my-portfolio_app_page_tsx_05woxaw._.js
+++ .next/dev/static/chunks/Desktop_google antigravity_my-portfolio_app_page_tsx_05woxaw._.js
@@ -1,5 +1,5 @@
 (globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
     script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/Desktop_google antigravity_my-portfolio_0nyshp~._.js","static/chunks/13y~_next_dist_compiled_react_0f68s5_._.js"],
+    chunks: ["static/chunks/Desktop_google antigravity_my-portfolio_0nyshp~._.js","static/chunks/13y~_next_08jys2~._.js"],
     source: "dynamic"
 });
